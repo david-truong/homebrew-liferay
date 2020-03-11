4205d239e6346365bf2b83fdae5a5635025d1976
David Truong <david.truong@liferay.com>
2020-03-10 23:41:13 -0500

    update gw to 1.0.3

--- Formula/gw.rb
+++ Formula/gw.rb
@@ -1,15 +1,16 @@
 class Gw < Formula
   desc "This project allows you to always call your gradle wrapper if it detects it"
   homepage "https://github.com/david-truong/gw"
-  url "https://github.com/david-truong/gw/archive/v1.0.2.tar.gz"
-  sha256 "7033091a6783fa351ca0e4a415500040627ec9a6ce31f353d7809786654af13c"
+  url "https://github.com/david-truong/gw/archive/v1.0.3.tar.gz"
+  sha256 "e5a29590496d44f59767672e7b00e2cf5bb1df5ccd5bc6889218df363b0a8a33"
 
   depends_on :java => "1.7+"
 
   def install
     system "./gradlew", "build"
+    system "./gradlew", "nativeImage"
     inreplace "brew/gw", "##PREFIX##", "#{prefix}"
-  	prefix.install "build/libs/gw.jar"
+  	prefix.install "build/graal/gw"
   	bin.install "brew/gw"
   end
 end
