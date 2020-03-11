2832cd6bece0ccc84206505524f332285ee2356c
David Truong <david.truong@liferay.com>
2020-03-11 00:16:05 -0500

    depends on jdk8

--- Formula/gw.rb
+++ Formula/gw.rb
@@ -4,7 +4,7 @@ class Gw < Formula
   url "https://github.com/david-truong/gw/archive/v1.0.3.tar.gz"
   sha256 "e5a29590496d44f59767672e7b00e2cf5bb1df5ccd5bc6889218df363b0a8a33"
 
-  depends_on :java => "1.7+"
+  depends_on :java => "1.8+"
 
   def install
     system "./gradlew", "build", "nativeImage"
