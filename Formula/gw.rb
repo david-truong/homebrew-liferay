3a265c436a2fe8865a0ad500605f219d5c61ebc2
David Truong <david.truong@liferay.com>
2020-12-10 09:57:00 -0600

    use gw 1.0.4

--- Formula/gw.rb
+++ Formula/gw.rb
@@ -1,14 +1,14 @@
 class Gw < Formula
   desc "This project allows you to always call your gradle wrapper if it detects it"
   homepage "https://github.com/david-truong/gw"
-  url "https://github.com/david-truong/gw/archive/v1.0.3.tar.gz"
-  sha256 "e5a29590496d44f59767672e7b00e2cf5bb1df5ccd5bc6889218df363b0a8a33"
+  url "https://github.com/david-truong/gw/archive/v1.0.4.tar.gz"
+  sha256 "54cf18319271b02e7c9cf47aa71e9b74b218c9978ec98c5fe5a67cc1bc4bdaa9"
 
   depends_on "openjdk@8"
 
   def install
     ENV["GRADLE_USER_HOME"] = buildpath/".brew_home"
-    system "./gradlew", "build", "nativeImage"
+    system "./gradlew", "build"
     inreplace "brew/gw", "##PREFIX##", "#{prefix}"
   	prefix.install "build/graal/gw"
   	bin.install "brew/gw"
