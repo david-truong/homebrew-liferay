e214790a0c28bfe36307b2ce2171e7ce378a6c83
David Truong <david.truong@liferay.com>
2022-03-16 22:19:09 -0500

    use v1.0.6 of gw

--- Formula/gw.rb
+++ Formula/gw.rb
@@ -1,8 +1,8 @@
 class Gw < Formula
   desc "This project allows you to always call your gradle wrapper if it detects it"
   homepage "https://github.com/david-truong/gw"
-  url "https://github.com/david-truong/gw/archive/v1.0.5.tar.gz"
-  sha256 "0e4ee6f4066ca9218909f54d23e1484d876584439c37c9aa6b6c7f416793237d"
+  url "https://github.com/david-truong/gw/archive/v1.0.6.tar.gz"
+  sha256 "1fdeb6a3fcd94b93a47a32620361f756ca4d049b61d55b65bb45583ca04ee8df"
 
   depends_on "openjdk"
 
