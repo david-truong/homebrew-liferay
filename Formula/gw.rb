fecedca9e4da3161c89586e9666cc485cfd667c2
David Truong <david.truong@liferay.com>
2022-03-16 21:51:17 -0500

    openjdk@8 doesn't work well on m1 macs

--- Formula/gw.rb
+++ Formula/gw.rb
@@ -4,7 +4,7 @@ class Gw < Formula
   url "https://github.com/david-truong/gw/archive/v1.0.5.tar.gz"
   sha256 "0e4ee6f4066ca9218909f54d23e1484d876584439c37c9aa6b6c7f416793237d"
 
-  depends_on "openjdk@8"
+  depends_on "openjdk"
 
   def install
     ENV["GRADLE_USER_HOME"] = buildpath/".brew_home"
