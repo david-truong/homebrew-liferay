022310202d272432fb45c288b8994cd7aba10130
David Truong <david.truong@liferay.com>
2022-03-22 01:10:33 -0500

    update blade to 4.10.0

--- Formula/blade.rb
+++ Formula/blade.rb
@@ -1,8 +1,8 @@
 class Blade < Formula
   desc "The Blade CLI is the easiest way for Liferay developers to create new Liferay modules."
   homepage "https://github.com/liferay/liferay-blade-cli"
-  url "https://releases.liferay.com/tools/blade-cli/3.2.0.201810082311/blade.jar"
-  sha256 "47a38378b93ca0c6e7751842fb70111bbffb750be9e7a3ebe42b7906f8d9b6ff"
+  url "https://search.maven.org/remotecontent?filepath=com/liferay/blade/com.liferay.blade.cli/4.0.10/com.liferay.blade.cli-4.0.10.jar"
+  sha256 "bfd869e625b51cb2ad2f6a79602ca777dfaba29afefa2128fb5a32bc41dd7bb8"
 
   depends_on "openjdk@11"
 
@@ -13,7 +13,7 @@ class Blade < Formula
       f.puts "java -jar #{prefix}/com.liferay.blade.cli.jar \"$@\""
     end
 
-  	prefix.install "blade.jar" => "com.liferay.blade.cli.jar"
+  	prefix.install "com.liferay.blade.cli-#{version}.jar" => "com.liferay.blade.cli.jar"
   	bin.install "blade"
   end
 end
